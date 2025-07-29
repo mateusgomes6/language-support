0fa821f1ea6146c93026f2044405bcff479dc29c
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-07-29 18:24:07 -0300

    feat: add tokenize medical

--- scripts/tokenize.py
+++ scripts/tokenize.py
@@ -16,3 +16,9 @@ def tokenize_legal(text):
         else:
             tokens.append(token.text)
     return ' '.join(tokens)
+
+def tokenize_medical(text):
+    medical_terms = ['tumor benigno', 'hipertensão arterial']
+    for term in medical_terms:
+        text = text.replace(term, term.replace(' ', '_'))
+    return word_tokenize(text, language='portuguese')
