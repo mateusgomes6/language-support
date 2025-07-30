f9dd2f4d2c3d867bea31262a8d3c5d939dbd8633
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-07-30 17:06:51 -0300

    fix: path from juridico.txt

--- scripts/2_tokenize.py
+++ scripts/2_tokenize.py
@@ -2,8 +2,8 @@ from scripts.clean_normalize import clean_text
 from pathlib import Path
 
 def process():
-    clean_juridico = Path('data/processed/cleaned/juridico.txt').read_text().splitlines()
+    clean_juridico = Path('data/raw/juridico.txt').read_text().splitlines()
     
     processed_terms = [f"JUR_{termo}" for termo in clean_juridico]
     
-    Path('data/processed/tokenized/juridico.txt').write_text('\n'.join(processed_terms))
+    Path('data/raw/juridico.txt').write_text('\n'.join(processed_terms))
