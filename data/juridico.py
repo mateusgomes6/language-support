d3c7921016c7141e2d41ce4bf7c25c6081695fc4
Mateus Gomes de carvalho <mateusgomesdc@hotmail.com>
2025-07-30 16:24:35 -0300

    remove: generate medical and juridical terms

--- data/juridico.py
+++
@@ -1,9 +0,0 @@
-import request
-from bs4 import BeautifulSoup
-
-def extract_legal_terms():
-    url = "https://www.planalto.gov.br/ccivil_03/_Ato2015-2018/2015/Lei/L13146.htm"
-    response = request.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    terms = [tag.text.strip() for tag in soup.find_all('span', class_='termo') if tag.text.strip()]
-    return terms
